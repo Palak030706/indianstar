6b1d5cf2cea7df7aac8dc3d6647549a392762f9e
Palak030706 <guptapalak1803@gmail.com>
2024-07-16 11:41:10 +0000

    changes

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,3 +1,15 @@
-export function onRequest(context) {
-  return new Response("Hello, world!")
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event.request))
+})
+
+async function handleRequest(request) {
+  const url = new URL(request.url)
+
+  if (url.pathname === '/store') {
+      const { key, value } = await request.json()
+      await COUNTER.put(key, value)
+      return new Response('Stored successfully', { status: 200 })
+  }
+
+  return new Response('Not found', { status: 404 })
 }
