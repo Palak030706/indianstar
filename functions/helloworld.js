6dddb3f02dc1499b58d2f327ab2f5cbd8f539e36
Palak030706 <guptapalak1803@gmail.com>
2024-07-17 06:48:37 +0000

    changes

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,6 +1,6 @@
 export async function onRequest(context) {
   const {COUNTER_NAMESPACE} = context.env;
   let counterValue = 0;
-  await COUNTER_NAMESPACE.put( 'counter',counterValue);
+  await COUNTER.put( 'counter',counterValue);
   return new Response("counterValue");
 }
