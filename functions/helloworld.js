d4bb03c6fb639317f9f3409f24f11232937f72d9
Palak030706 <guptapalak1803@gmail.com>
2024-07-17 07:29:37 +0000

    counter

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,5 +1,8 @@
 export async function onRequest(context) {
-  const { MYKV} = context.env;
-  const counterValue = await MYKV.get('Counter');
-  return new Response(counterValue);
-} 
+  const { MYKV } = context.env;
+  const counterValue = await MYKVSTORE.get('Counter');
+  let counterValue = await MYKVSTORE.get('Counter');
+  counterValue = parseInt(counterValue) || 0;
+  counterValue += 1;
+  await MYKVSTORE.put('Counter', counterValue);
+}
