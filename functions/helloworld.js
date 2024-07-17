4bd5218d7fc9ccbe514e1cfabcf686330551e8b7
Palak030706 <guptapalak1803@gmail.com>
2024-07-17 06:04:13 +0000

    CHANGES

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,31 +1,4 @@
-addEventListener('fetch', event => {
-  event.respondWith(handleRequest(event.request))
-})
-
-async function handleRequest(request) {
-  const url = new URL(request.url)
-  const path = url.pathname
-
-  // Initialize the KV namespace
-  const kv = COUNTER_NAMESPACE
-
-  // Get the current count from KV
-  let count = await kv.get(path)
-
-  // If there's no count yet, initialize it
-  if (count === null) {
-    count = 0
-  } else {
-    count = parseInt(count)
-  }
-
-  // Increment the count
-  count++
-
-  // Store the new count in KV
-  await kv.put(path, count.toString())
-
-  // Return the count as the response
-  return new Response(`This page has been viewed ${count} times.`)
+export function onRequest(context) {
+  return new Response("COUNTER_NAMESPACE")
 }
   
