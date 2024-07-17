6c444d4aa81089f246c0309ff3961189895a9e41
Palak030706 <guptapalak1803@gmail.com>
2024-07-17 07:33:09 +0000

    changes

--- functions/helloworld.js
+++ functions/helloworld.js
@@ -1,8 +1,5 @@
 export async function onRequest(context) {
-  const { MYKV } = context.env;
-  const counterValue = await MYKVSTORE.get('Counter');
-  let counterValue = await MYKVSTORE.get('Counter');
-  counterValue = parseInt(counterValue) || 0;
-  counterValue += 1;
-  await MYKVSTORE.put('Counter', counterValue);
+  const { MYKVSTORE } = context.env;
+  const counterValue = await MYKVS.get('Counter');
+  return new Response(counterValue);
 }
