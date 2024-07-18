a8c91a524e1c6ea48d125fcdf4fa88d47661c6c2
Palak030706 <guptapalak1803@gmail.com>
2024-07-18 11:43:25 +0000

    commit

--- functions/comments.js
+++ functions/comments.js
@@ -6,22 +6,22 @@ export async function onRequest(context) {
     console.log(comment);
 
     if (comment) {
-        console.log("line 2")
+        console.log("line 1")
         try {
             
             // Store the message in KV store with a unique key
-            await MYKVSTORE.put('comment', comment);
-            console.log("line 8")
+            await MYKV.put('comment', comment);
+            console.log("line 7")
 
             return new Response('Message saved successfully', { status: 200 });
-            console.log("line 9")
+            console.log("line 8")
         } catch (error) {
-            console.log("line 10")
+            console.log("line 9")
             return new Response('Invalid request', { status: 400 });
-            console.log("line 11")
+            console.log("line 10")
         }
     } else {
-        console.log("line 12")
+        console.log("line 11")
         return new Response('Invalid action', { status: 400 });
     }
 }
